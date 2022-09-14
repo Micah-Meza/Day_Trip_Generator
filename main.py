73dcf0ca447bb1a54bf6a4b7e0f008aa32a64e41
Micah <airrenno@yahoo.com>
2022-09-13 20:59:17 -0500

    Updated code and fixed bugs

--- main.py
+++ main.py
@@ -19,46 +19,43 @@ import random
 #       each function should do just one thing!
 ##################################################################################################################################
 
-# These def allows for randome choices.
 
-
-
-
-
-
-
-
+destinations = ["Canada", "Spain", "Tokyo", "London", "Fiji Island"]
+restaurants = ["Noma", "Geranium", "Asador Etxebarri", "Disfrutar Frantzén", "McDonalds"]
+transportation_selections = ["Rolls-Royce Phantom", "Bugatti Chiron", "Private Jet", "Public Plane", "Helicopter"]
+entertainment_selections = ["Skydiving", "Concert", "Mountain Climbing", "Space Walk", "Escape Island"]
 
 def run_day_trip_plan():
-    destinations = ["Canada", "Spain", "Tokyo", "London", "Fiji Island"]
-    restaurants = ["Noma", "Geranium", "Asador Etxebarri", "Disfrutar Frantzén", "McDonalds"]
-    transportation_selections = ["Rolls-Royce Phantom", "Bugatti Chiron", "Private Jet", "Public Plane", "Helicopter"]
-    entertainment_selections = ["Skydiving", "Concert", "Mountain Climbing", "Space Walk", "Escape Island"]
-    des = generate_random_plan(destinations)
-    res = generate_random_plan(restaurants)
-    tra = generate_random_plan(transportation_selections)
-    ent = generate_random_plan(entertainment_selections)
+    destination = generate_random_plan(destinations)
+    restaurant = generate_random_plan(restaurants)
+    transportation = generate_random_plan(transportation_selections)
+    entertainment = generate_random_plan(entertainment_selections)
     trip_plan = [des, res, tra, ent]
     print_full_trip(trip_plan)
-
-
-    
-    selected_choice = reselect_trip(choice)
-
-
-
-
-
-
-
     print()
+    trip_confirmation(destination, restaurant, transportation, entertainment)
+    
 
+def trip_confirmation(destination, restaurant, transportation, entertainment):
+        trip_plan = [destination, restaurant, transportation, entertainment]
+        cont = True
+        while cont == True:
+            select = input("Are you satisfied with the selections? (Y/N) ")
+            if select == "n":
+                trip_option = input("Which would you like to change: Destination, Resataurant, Transportation, or Entertainment? ")
+                print()
+                reselect_trip(trip_option, trip_plan)
+                cont = True              
+            elif select == "y":
+                print()
+                print("Here's is your final list: ")
+                print_full_trip(trip_plan)
+                print()
+                cont = False
+                
 
 def print_full_trip(trip_option):
-    print("Destination: {}\nRestaurant {}\nTransportation {}\nEntertainment {}\n.".format(trip_option))
-    choice = input("Are you happy with the selections? (Y/N) ")
-    
-    return choice
+    print("Destination: {}\nRestaurant: {}\nTransportation: {}\nEntertainment: {}".format(trip_option[0],trip_option[1],trip_option[2],trip_option[3]))
 
 
 def generate_random_plan(item_lists):
@@ -66,201 +63,44 @@ def generate_random_plan(item_lists):
     return item_lists
 
 
-def choice_option(current_trip, trip_option):
-    invalid = True
-    while invalid == True:
-        choice = input("Would like to keep entertainment selection? (Y/N) ")
-        if choice == "Y" or choice == "y":
-            print("Confirmed")
+def reselect_trip(trip_option, new):
+        if trip_option == "Destination" or trip_option == "destination":
+            new = new
+            destination = generate_random_plan(destinations)
+            new.pop(0)
+            new.insert(0, destination)
+            print_full_trip(new)
             print()
-            answer = False
-            invalid = False
-        elif choice == "N" or choice == "n":
 
-            print("Let Us please reselect this for you. ")
-            answer = True
-            invalid = False
-        elif choice != "Y" or choice != "y" or choice != "N" or choice != "n":
+        elif trip_option == "Restaurant" or trip_option == "restaurant":
+            new = new
+            restaurant = generate_random_plan(restaurants)
+            new.pop(1)
+            new.insert(1, restaurant)
+            print_full_trip(new)
+            print()
+
+        elif trip_option == "Transportation" or trip_option == "transportation":
+            new = new
+            transportation = generate_random_plan(transportation_selections)
+            new.pop(2)
+            new.insert(2, transportation)
+            print_full_trip(new)
+            print()
+
+        elif trip_option == "Entertainment" or trip_option == "entertainment":
+            new = new
+            entertainment = generate_random_plan(entertainment_selections)
+            new.pop(3)
+            new.insert(3, entertainment)
+            print_full_trip(new)
+            print()
+
+        else:
             print("Invalid choice please try again! ")
-
-            invalid = True
-    return answer
-
-
-def reselect_trip(current_trip, options):
-    
-
-
-
-
-
-
-
-
-
-    print()
 
 
 run_day_trip_plan()
 
 
 
-
-
-
-
-
-
-# def trip_destination(places):
-#     places = random.choice(places)
-#     return places
-
-# def trip_restaurants(foods):
-#     foods = random.choice(foods)
-#     return foods
-
-# def trip_tranportation(cars):
-#     cars = random.choice(cars)
-#     return cars
-
-# def trip_entertainment(activities):
-#     activities = random.choice(activities)
-#     return activities
-
-# These def process the user input and validates the correct choice.
-# def destination_choice(answer):
-#     invalid = True
-#     while invalid == True:
-#         choice = input("Would you like to keep this destination selection? (Y/N) ")
-#         if choice == "Y" or choice == "y":
-#             print("Confirmed")
-#             print()
-#             answer = False
-#             invalid = False
-#         elif choice == "N" or choice == "n":
-#             print("Let Us please reselect this for you. ")
-#             answer = True
-#             invalid = False
-#         elif choice != "Y" or choice != "y" or choice != "N" or choice != "n":
-#             print("Invalid choice please try again! ")
-#             invalid = True
-#     return answer
-
-
-# def transportation_choice(answer):
-#     invalid = True
-#     while invalid == True:
-#         choice = input("Do you wish to keep this transportation selection? (Y/N) ")
-#         if choice == "Y" or choice == "y":
-#             print("Confirmed")
-#             print()
-#             answer = False
-#             invalid = False
-#         elif choice == "N" or choice == "n":
-#             print("Let Us please reselect this for you. ")
-#             answer = True
-#             invalid = False
-#         elif choice != "Y" or choice != "y" or choice != "N" or choice != "n":
-#             print("Invalid choice please try again! ")
-#             print()
-#             invalid = True
-#     return answer
-
-
-# def restaurants_choice(answer):
-#     invalid = True
-#     while invalid == True:
-#         choice = input("Would you like to keep this restaurant choice? (Y/N) ")
-#         if choice == "Y" or choice == "y":
-#             print("Confirmed")
-#             print()
-#             answer = False
-#             invalid = False
-#         elif choice == "N" or choice == "n":
-#             print("Let Us please reselect this for you. ")
-#             answer = True
-#             invalid = False
-#         elif choice != "Y" or choice != "y" or choice != "N" or choice != "n":
-#             print("Invalid choice please try again! ")
-#             invalid = True
-#     return answer
-
-
-# def entertainment_choice(answer):
-#     invalid = True
-#     while invalid == True:
-#         choice = input("Would like to keep entertainment selection? (Y/N) ")
-#         if choice == "Y" or choice == "y":
-#             print("Confirmed")
-#             print()
-#             answer = False
-#             invalid = False
-#         elif choice == "N" or choice == "n":
-
-#             print("Let Us please reselect this for you. ")
-#             answer = True
-#             invalid = False
-#         elif choice != "Y" or choice != "y" or choice != "N" or choice != "n":
-#             print("Invalid choice please try again! ")
-
-#             invalid = True
-#     return answer
-
-# destinations = ["Canada", "Spain", "Tokyo", "London", "Fiji Island"]
-# restaurants = ["Noma", "Geranium", "Asador Etxebarri", "Disfrutar Frantzén", "McDonalds"]
-# transportation_selections = ["Rolls-Royce Phantom", "Bugatti Chiron", "Private Jet", "Public Plane", "Helicopter"]
-# entertainment_selections = ["Skydiving", "Concert", "Mountain Climbing", "Space Walk", "Escape Island"]
-
-
-
-# def run_day_trip_plan():
-
-
-# The main program.
-# cont = True
-# while cont == True:
-#     print()
-#     trip_location = trip_destination(destinations)
-#     print("The destination choosen is {}!".format(trip_location))
-#     cont = destination_choice(cont)
-#     if cont == True:
-#         destinations.remove(trip_location)
-#     elif cont == False:
-#         print("********************************************************")
-        
-# cont_1 = True
-# while cont_1 == True:
-#     print()
-#     trip_ride = trip_tranportation(transportation_selections)
-#     print("Your transportation would be a {}!".format(trip_ride))
-#     cont_1 = transportation_choice(cont_1)
-#     if cont_1 == True:
-#         transportation_selections.remove(trip_ride)
-#     elif cont_1 == False:
-#         print("********************************************************")
-
-# cont_2 = True
-# while cont_2  == True:
-#     print()
-#     trip_food = trip_restaurants(restaurants)
-#     print("You will dine at {}!".format(trip_food))
-#     cont_2  = restaurants_choice(cont_2)
-#     if cont_2  == True:
-#         restaurants.remove(trip_food)
-#     elif cont_2  == False:
-#         print("********************************************************")
-
-# cont_3 = True
-# while cont_3 == True:
-#     print()
-#     trip_fun = trip_entertainment(entertainment_selections)
-#     print("The activity choosen is a {}!".format(trip_fun))
-#     cont_3 = entertainment_choice(cont_3 )
-#     if cont_3 == True:
-#         entertainment_selections.remove(trip_fun)
-#     elif cont_3 == False:
-#         print("********************************************************")
-
-# print()
-# print("Your entertainment will be a {} and you will travel to {} while being transported by a(n) {} and dining at {}.".format(trip_fun, trip_location, trip_ride, trip_food))
-# print() 
